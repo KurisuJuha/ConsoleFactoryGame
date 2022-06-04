ebea520663f7f48d323e6ac69ee7341c9646f068
KYapp <82925909+kukiyomenaisapp@users.noreply.github.com>
2022-06-04 23:11:27 +0900

    UpdateとStartを追加

--- ConsoleFactoryGame/Program.cs
+++ ConsoleFactoryGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace KYapp.ConsoleFactoryGame
 {
@@ -7,6 +8,26 @@ namespace KYapp.ConsoleFactoryGame
         public static void Main(string[] args)
         {
             Console.WriteLine("GameStart");
+
+            Start();
+
+            while (true)
+            {
+                Update();
+                Task task = Task.Delay(1000);
+                task.Wait();
+            }
+            
+        }
+
+        public static void Start()
+        {
+            
+        }
+        
+        public static void Update()
+        {
+            Console.WriteLine("aaaa");
         }
     }
 }
