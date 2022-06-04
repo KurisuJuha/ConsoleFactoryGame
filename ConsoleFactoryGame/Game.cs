5e6d493612a7f9587597a4af2fe214cfe698592b
KYapp <82925909+kukiyomenaisapp@users.noreply.github.com>
2022-06-05 00:02:12 +0900

    コマンドの基盤ができた

---
+++ ConsoleFactoryGame/Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace KYapp.ConsoleFactoryGame
+{
+    public static class Game
+    {
+        private static int data;
+        
+        
+        public static void Main(string[] args)
+        {
+            BackGround(100, Start, Update);
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                
+                Console.Write(">>");
+                string command = Console.ReadLine();
+                
+                Console.ResetColor();
+                Parser.Parse(command);
+            }
+        }
+        
+        private static async Task BackGround(int span,Action start, Action update)
+        {
+            start();
+            
+            while (true)
+            {
+                await Task.Delay(span);
+                update();
+            }
+        }
+
+        private static void Start()
+        {
+            
+        }
+
+        private static void Update()
+        {
+            
+        }
+    }
+}
---
+++ ConsoleFactoryGame/Parser.cs
@@ -0,0 +1,33 @@
+﻿namespace KYapp.ConsoleFactoryGame;
+
+public static class Parser
+{
+    public static Dictionary<string, Action> Commands = new Dictionary<string, Action>();
+
+    public static void Parse(string command)
+    {
+        string[] _command = Split(command);
+        Action action;
+
+        if (_command.Length < 1) return;
+
+
+        if (Commands.TryGetValue(_command[0], out action))
+        {
+            action();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Error");
+            Console.ResetColor();
+        }
+    }
+
+    public static string[] Split(string command)
+    {
+        return command.Split(' ');
+    }
+    
+    
+}
