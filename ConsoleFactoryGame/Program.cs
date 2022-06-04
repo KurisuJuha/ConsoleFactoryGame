883554cc76921829dfe677dbb71a88f576e74798
KYapp <82925909+kukiyomenaisapp@users.noreply.github.com>
2022-06-04 22:56:01 +0900

    Init

---
+++ ConsoleFactoryGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KYapp.ConsoleFactoryGame
+{
+    public static class Game
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("GameStart");
+        }
+    }
+}
